62bad6cc74add34ce7cfe2d54cfdbc8a6e3ee6ad
CarlosPavajeau <cantte098@gmail.com>
2024-09-23 17:13:24 -0500

    feat: load ef configuration from assembly

--- Wishlist/Infrastructure/Persistence/EntityFramework/WishlistDbContext.cs
+++ Wishlist/Infrastructure/Persistence/EntityFramework/WishlistDbContext.cs
@@ -8,4 +8,11 @@ public class WishlistDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<WishlistProduct> Wishlists { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WishlistDbContext).Assembly);
+    }
 }
