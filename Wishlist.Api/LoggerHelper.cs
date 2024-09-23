209158beed7e5a47ab03164ecd71acbfd509c0d9
CarlosPavajeau <cantte098@gmail.com>
2024-09-23 12:29:03 -0500

    feat: add initial logger configuration

---
+++ Wishlist.Api/LoggerHelper.cs
@@ -0,0 +1,3 @@
+namespace Wishlist.Api;
+
+public record LoggerHelper();
--- Wishlist.Api/Program.cs
+++ Wishlist.Api/Program.cs
@@ -1,15 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Wishlist.Api;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSerilog();
+
+builder.Logging.AddSerilog();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/{id:int}", (int id, [FromServices] ILogger<LoggerHelper> logger) =>
+{
+    if (id == 1)
+    {
+        logger.LogInformation("Returning product with id {Id}", id);
+        return Results.Ok(new {Id = id, Name = "Product 1"});
+    }
+
+    logger.LogWarning("Product with id {Id} not found", id);
+    return Results.NotFound();
+});
 
 app.UseHttpsRedirection();
 
